767a3f70c7da67dd7ee6b76852e291c392e72ebe
Satoru Yoshihara <satoru.yoshihara@linecorp.com>
2017-09-11 21:44:13 +0900

    Implemented image carousel message

--- tests/LINEBot/SendTemplateTest.php
+++ tests/LINEBot/SendTemplateTest.php
@@ -23,6 +23,8 @@ use LINE\LINEBot\Constant\ActionType;
 use LINE\LINEBot\Constant\MessageType;
 use LINE\LINEBot\Constant\TemplateType;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
 use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
 use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
 use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
@@ -143,6 +145,81 @@ class SendTemplateTest extends \PHPUnit_Framework_TestCase
                         new PostbackTemplateActionBuilder('postback label2', 'post=back2', 'extend text'),
                         new MessageTemplateActionBuilder('message label', 'test message'),
                         new UriTemplateActionBuilder('uri label', 'https://example.com'),
+                    ]
+                )
+            )
+        );
+
+        $this->assertEquals(200, $res->getHTTPStatus());
+        $this->assertTrue($res->isSucceeded());
+        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
+    }
+
+
+    public function testImageCarouselTemplate()
+    {
+        $mock = function ($testRunner, $httpMethod, $url, $data) {
+            /** @var \PHPUnit_Framework_TestCase $testRunner */
+            $testRunner->assertEquals('POST', $httpMethod);
+            $testRunner->assertEquals('https://api.line.me/v2/bot/message/push', $url);
+
+            $testRunner->assertEquals('DESTINATION', $data['to']);
+            $testRunner->assertEquals(1, count($data['messages']));
+
+            $message = $data['messages'][0];
+            $testRunner->assertEquals(MessageType::TEMPLATE, $message['type']);
+            $testRunner->assertEquals('alt test', $message['altText']);
+
+            $template = $message['template'];
+            $testRunner->assertEquals(TemplateType::IMAGE_CAROUSEL, $template['type']);
+
+            $columns = $template['columns'];
+            $testRunner->assertEquals(4, count($columns));
+            $testRunner->assertEquals('https://example.com/image1.png', $columns[0]['imageUrl']);
+            $testRunner->assertEquals(ActionType::POSTBACK, $columns[0]['action']['type']);
+            $testRunner->assertEquals('postback label', $columns[0]['action']['label']);
+            $testRunner->assertEquals('post=back', $columns[0]['action']['data']);
+
+            $testRunner->assertEquals('https://example.com/image2.png', $columns[1]['imageUrl']);
+            $testRunner->assertEquals(ActionType::POSTBACK, $columns[1]['action']['type']);
+            $testRunner->assertEquals('postback label2', $columns[1]['action']['label']);
+            $testRunner->assertEquals('post=back2', $columns[1]['action']['data']);
+
+            $testRunner->assertEquals('https://example.com/image3.png', $columns[2]['imageUrl']);
+            $testRunner->assertEquals(ActionType::MESSAGE, $columns[2]['action']['type']);
+            $testRunner->assertEquals('message label', $columns[2]['action']['label']);
+            $testRunner->assertEquals('test message', $columns[2]['action']['text']);
+
+            $testRunner->assertEquals('https://example.com/image4.png', $columns[3]['imageUrl']);
+            $testRunner->assertEquals(ActionType::URI, $columns[3]['action']['type']);
+            $testRunner->assertEquals('uri label', $columns[3]['action']['label']);
+            $testRunner->assertEquals('https://example.com', $columns[3]['action']['uri']);
+
+            return ['status' => 200];
+        };
+        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => 'CHANNEL-SECRET']);
+        $res = $bot->pushMessage(
+            'DESTINATION',
+            new LINEBot\MessageBuilder\TemplateMessageBuilder(
+                'alt test',
+                new ImageCarouselTemplateBuilder(
+                    [
+                        new ImageCarouselColumnTemplateBuilder(
+                            'https://example.com/image1.png',
+                            new PostbackTemplateActionBuilder('postback label', 'post=back')
+                        ),
+                        new ImageCarouselColumnTemplateBuilder(
+                            'https://example.com/image2.png',
+                            new PostbackTemplateActionBuilder('postback label2', 'post=back2', 'extend text')
+                        ),
+                        new ImageCarouselColumnTemplateBuilder(
+                            'https://example.com/image3.png',
+                            new MessageTemplateActionBuilder('message label', 'test message')
+                        ),
+                        new ImageCarouselColumnTemplateBuilder(
+                            'https://example.com/image4.png',
+                            new UriTemplateActionBuilder('uri label', 'https://example.com')
+                        ),
                     ]
                 )
             )
