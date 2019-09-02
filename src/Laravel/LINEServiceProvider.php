4e709835b93eca2edcbecc0b2dd3d2ab6eea27ba
moririnson <morimorim7180@gmail.com>
2019-09-03 01:56:58 +0900

    Add copy-right for laravel namespace

--- src/Laravel/LINEServiceProvider.php
+++ src/Laravel/LINEServiceProvider.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 namespace LINE\Laravel;
 
 use LINE\LINEBot;
@@ -15,7 +31,8 @@ class LINEServiceProvider extends \Illuminate\Support\ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/config/line-bot.php', 'line-bot'
+            __DIR__ . '/config/line-bot.php',
+            'line-bot'
         );
         $this->app->bind('linebot', function () {
             $httpClient = new CurlHTTPClient(config('line-bot.channel_access_token'));
