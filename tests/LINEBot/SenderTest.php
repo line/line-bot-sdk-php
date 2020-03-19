26b176b276b7964d2c032bc3fde32eeac63b98ba
snmatsui <matsui@iystyle.jp>
2020-03-19 16:02:41 +0900

    comment: update linecorp copyright 2020 on new classes

--- tests/LINEBot/SenderTest.php
+++ tests/LINEBot/SenderTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2018 LINE Corporation
+ * Copyright 2020 LINE Corporation
  *
  * LINE Corporation licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
