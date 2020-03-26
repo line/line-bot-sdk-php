76c16e8b09de22782c3d7c1b437c01da2730f722
moririnson <morimorim7180@gmail.com>
2020-03-26 17:20:01 +0900

    Add audience management

--- src/LINEBot/HTTPClient/CurlHTTPClient.php
+++ src/LINEBot/HTTPClient/CurlHTTPClient.php
@@ -85,6 +85,20 @@ class CurlHTTPClient implements HTTPClient
     }
 
     /**
+     * Sends PUT request to LINE Messaging API.
+     *
+     * @param string $url Request URL.
+     * @param array $data Request body.
+     * @param array|null $headers Request headers.
+     * @return Response Response of API request.
+     */
+    public function put($url, array $data, array $headers = null)
+    {
+        $headers = is_null($headers) ? ['Content-Type: application/json; charset=utf-8'] : $headers;
+        return $this->sendRequest('PUT', $url, $headers, $data);
+    }
+
+    /**
      * Sends DELETE request to LINE Messaging API.
      *
      * @param string $url Request URL.
