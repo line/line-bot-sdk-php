55cc3dcf21e648a8546ef3e64d62a664f57fe496
moririnson <morimorim7180@gmail.com>
2021-03-03 12:44:51 +0900

    Update GetFollowerIdsTest.php

--- tests/LINEBot/GetFollowerIdsTest.php
+++ tests/LINEBot/GetFollowerIdsTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2020 LINE Corporation
+ * Copyright 2021 LINE Corporation
  *
  * LINE Corporation licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
