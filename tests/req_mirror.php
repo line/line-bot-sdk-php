d5539aacad8e0827b50f3987a5cae86019810eeb
smatsui <snmatsui@users.noreply.github.com>
2021-03-23 18:11:53 +0900

    fix: typo

--- tests/req_mirror.php
+++ tests/req_mirror.php
@@ -20,7 +20,7 @@ $body = file_get_contents('php://input');
 
 /** responseDelay url paramter define delay of response.(millisecond) */
 if (isset($_GET["responseDelay"]) && is_numeric($_GET["responseDelay"])) {
-    usleep($_GET["responseDelay"]) * 1000);
+    usleep($_GET["responseDelay"] * 1000);
 }
 
 header('Content-Type: application/json');
