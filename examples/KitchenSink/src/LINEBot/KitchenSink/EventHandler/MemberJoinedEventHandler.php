cef9479bc2572a95eb151494b6b850ccaa9142ee
Yuta Euchi <63095160+eucyt@users.noreply.github.com>
2025-03-25 18:41:20 +0900

    Add memberJoinedEventHandler in KitchenSink (#678)
    I have added handler for MemberJoinedEvent webhook in KitchenSink which
    is SDK example.
    If you launch KitchenSink and joined some user to LINE group joined the
    Official Account, the Official Account will send messages like this.
    
    
    ![IMG_0058](https://github.com/user-attachments/assets/8feb05a1-8345-4885-b841-f6c1e28d2e06)

---
+++ examples/KitchenSink/src/LINEBot/KitchenSink/EventHandler/MemberJoinedEventHandler.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Copyright 2025 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace LINE\LINEBot\KitchenSink\EventHandler;
+
+use LINE\Clients\MessagingApi\Api\MessagingApiApi;
+use LINE\Clients\MessagingApi\Model\ReplyMessageRequest;
+use LINE\Clients\MessagingApi\Model\TextMessage;
+use LINE\Constants\MessageType;
+use LINE\LINEBot\KitchenSink\EventHandler;
+use LINE\Webhook\Model\GroupSource;
+use LINE\Webhook\Model\JoinEvent;
+use LINE\Webhook\Model\MemberJoinedEvent;
+use LINE\Webhook\Model\RoomSource;
+
+class MemberJoinedEventHandler implements EventHandler
+{
+    /** @var MessagingApiApi $bot */
+    private $bot;
+    /** @var \Psr\Log\LoggerInterface $logger */
+    private $logger;
+    /** @var MemberJoinedEvent $memberJoinedEvent */
+    private $memberJoinedEvent;
+
+    /**
+     * JoinEventHandler constructor.
+     * @param MessagingApiApi $bot
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param MemberJoinedEvent $memberJoinedEvent
+     */
+    public function __construct(MessagingApiApi $bot, \Psr\Log\LoggerInterface $logger, MemberJoinedEvent $memberJoinedEvent)
+    {
+        $this->bot = $bot;
+        $this->logger = $logger;
+        $this->memberJoinedEvent = $memberJoinedEvent;
+    }
+
+    /**
+     * @throws LINEBot\Exception\InvalidEventSourceException
+     * @throws \ReflectionException|\LINE\Clients\MessagingApi\ApiException
+     */
+    public function handle()
+    {
+        $source = $this->memberJoinedEvent->getSource();
+        if ($source instanceof GroupSource) {
+            $id = $source->getGroupId();
+        } elseif ($source instanceof RoomSource) {
+            $id = $source->getRoomId();
+        } else {
+            $this->logger->error("Unknown event type");
+            return;
+        }
+
+        $joinedMembers = $this->memberJoinedEvent->getJoined()->getMembers();
+        $joinedMemberIds = array_map(function ($member) {
+            return $member["userId"];
+        }, $joinedMembers);
+
+        $request = new ReplyMessageRequest([
+            'replyToken' => $this->memberJoinedEvent->getReplyToken(),
+            'messages' => [
+                new TextMessage([
+                    'type' => MessageType::TEXT,
+                    'text' => sprintf('%s joined. %s %s', implode(", ", $joinedMemberIds), $this->memberJoinedEvent->getType(), $id),
+                ]),
+            ],
+        ]);
+        $this->bot->replyMessage($request);
+    }
+}
